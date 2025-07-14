710cb82fce830ae2dc38d99f57e814a068386b89
Jungkihong07 <hnn07185@gmail.com>
2025-07-14 21:26:04 +0900

    디버깅 환경 조성 및 python 환경 통일

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,20 @@
+import os
+
 # main.py
+if os.getenv("DEBUG", "false").lower() == "true":
+    import debugpy
+
+    try:
+        debugpy.listen(("0.0.0.0", 5678))
+        print("✅ VSCode 디버거 대기 중 (5678 포트)...")
+        # debugpy.wait_for_client()  # 필요시
+    except RuntimeError as e:
+        if "Address already in use" in str(e):
+            print("⚠️ debugpy 포트 5678 이미 사용 중. 중복 listen 시도 무시.")
+        else:
+            raise
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
