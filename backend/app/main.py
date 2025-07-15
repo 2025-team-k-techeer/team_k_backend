3e55cb0f74d3e495c4c28d5b9829985d1666ca1b
Jungkihong07 <hnn07185@gmail.com>
2025-07-15 17:17:15 +0900

    디버깅 issue 해결

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,19 +10,19 @@ settings = get_settings()
 
 app = FastAPI()
 
-# main.py
-if os.getenv("DEBUG", "false").lower() == "true":
-    import debugpy
+# # main.py
+# if os.getenv("DEBUG", "false").lower() == "true":
+#     import debugpy
 
-    try:
-        debugpy.listen(("0.0.0.0", 5678))
-        print("✅ VSCode 디버거 대기 중 (5678 포트)...")
-        # debugpy.wait_for_client()  # 필요시
-    except RuntimeError as e:
-        if "Address already in use" in str(e):
-            print("⚠️ debugpy 포트 5678 이미 사용 중. 중복 listen 시도 무시.")
-        else:
-            raise
+#     try:
+#         debugpy.listen(("0.0.0.0", 5678))
+#         print("✅ VSCode 디버거 대기 중 (5678 포트)...")
+#         debugpy.wait_for_client()  # 필요시
+#     except RuntimeError as e:
+#         if "Address already in use" in str(e):
+#             print("⚠️ debugpy 포트 5678 이미 사용 중. 중복 listen 시도 무시.")
+#         else:
+#             raise
 
 
 @app.get("/test/redis")
