2aaf101b109f6cb19382ffb986d5a4ddd24b05e2
Jungkihong07 <hnn07185@gmail.com>
2025-07-22 05:00:52 +0900

    roll back 성공 유후

--- backend/app/main.py
+++ backend/app/main.py
@@ -77,86 +77,12 @@
 # app/main.py
 
 # app/main.py
-import os
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from app.user.interface.controller import user_controller
 from app.interior.interface.controller import interior_controller
-import json
-import logging
-from prometheus_fastapi_instrumentator import Instrumentator
-from fastapi.middleware.cors import CORSMiddleware
 
-
-class CustomJsonFormatter(logging.Formatter):
-
-    # CustomJsonFormatter는 로그 메시지를 JSON 형식으로 포맷팅하는 사용자 정의 로거 포맷터입니다.
-
-    # Methods:
-    # 	format(record):
-    # 		주어진 로그 레코드를 JSON 형식으로 변환합니다.
-    # 		- record: 로그 레코드 객체로, 로그 메시지와 관련된 모든 정보를 포함합니다.
-
-    def format(self, record):
-        log_record = {
-            "timestamp": self.formatTime(record, self.datefmt),
-            "level": record.levelname,
-            "message": record.getMessage(),
-            "name": record.name,
-        }
-        return json.dumps(log_record)
-
-
-# programmatically create logs directory if it doesn't exist
-if not os.path.exists("./app/logs"):
-    os.makedirs("./app/logs", exist_ok=True)
-    os.chmod("./app/logs", 0o755)
 app = FastAPI()
-logger = logging.getLogger("uvicorn.access")
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler("./app/logs/fastapi.log")
-handler.setFormatter(CustomJsonFormatter())
-logger.addHandler(handler)
 
-
-@app.middleware("http")
-async def add_logging_middleware(request: Request, call_next):
-    """
-    HTTP 요청에 대한 로깅 미들웨어를 추가합니다.
-
-    이 미들웨어는 다음 작업을 수행합니다:
-    1. "uvicorn.access" 로거를 가져오고 INFO 레벨로 설정합니다.
-    2. 로그를 기록할 파일 핸들러를 추가하고, 사용자 정의 JSON 포맷터를 설정합니다.
-    3. 요청 메서드, URL, 상태 코드, 클라이언트 IP 주소와 같은 요청 및 응답 데이터를 JSON 형식으로 로깅합니다.
-
-    매개변수:
-    - request (Request): FastAPI 요청 객체.
-    - call_next (Callable): 다음 미들웨어 또는 엔드포인트를 호출하는 함수.
-
-    반환값:
-    - response: FastAPI 응답 객체.
-    """
-
-    response = await call_next(request)
-    log_data = {
-        "method": request.method,
-        "url": str(request.url),
-        "status_code": response.status_code,
-        "client": request.client.host,
-    }
-    logger.info(json.dumps(log_data))
-    return response
-
-
-# CORS 설정 for metrics and logging
-origins = ["*"]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-Instrumentator().instrument(app).expose(app)
 # 라우터 등록
 app.include_router(user_controller.router)
 app.include_router(interior_controller.router)
